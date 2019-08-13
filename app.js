4f8d48289f5bea590942b7fb2e8425f10112f3f5
emmanuel <emmanuel.igwe38@gmail.com>
2019-08-14 00:52:21 +0100

    tool

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname ,'views/index.html'));
+    res.sendFile(path.join(__dirname ,'/views/', '/index.html/'));
 });
 app.listen(3000, function(){
     debug(`listening on port  ${chalk.green('3000')}`)
