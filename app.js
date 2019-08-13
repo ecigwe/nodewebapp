3a02d5cc81f3886bda8961389843643bf1de65ed
emmanuel <emmanuel.igwe38@gmail.com>
2019-08-13 20:08:32 +0100

    ignore

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
-
+var chalk = require('chalk');
+var debug = require('debug');
+var morgan = require('morgan');
+var path = require('path');
 var app = express();
+app.use(morgan('tiny'));
+
 
 app.get('/', function(req, res){
-    res.send('hello from my library');
+    res.sendFile(path.join(__dirname ,'views/index.html'));
 });
 app.listen(3000, function(){
-    console.log('listening on port 3000')
+    debug(`listening on port  ${chalk.green('3000')}`)
 });
