ea123cac5ca06317c57e962873d5298238305f3a
emmanuel <emmanuel.igwe38@gmail.com>
2019-08-23 16:52:02 +0100

    ejx

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug');
 const morgan = require('morgan');
 const path = require('path');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -12,9 +13,16 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+  res.render('index',
+
+    {
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+      title: 'my library'
+    });
 });
 app.listen(port, () => {
   debug(`listening on port  ${chalk.green('port')}`);
